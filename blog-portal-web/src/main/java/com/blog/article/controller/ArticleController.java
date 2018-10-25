fed70a0977dedb5ec911b07fa8f8f0ea570d8ac8
mawenlong <15061886113@163.com>
2018-10-25 17:15:57 +0800

    模板修改

--- blog-portal-web/src/main/java/com/blog/article/controller/ArticleController.java
+++ blog-portal-web/src/main/java/com/blog/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.blog.article.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.blog.content.service.ArticleService;
 import com.blog.pojo.Article;
+import com.blog.pojo.PageResult;
 import java.util.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,15 @@ public class ArticleController {
     article.setPictureurl("url");
     article.setSummary("summary");
     articleService.insert(article);
+  }
+
+  @RequestMapping("/listPage")
+  public PageResult listPage(int page, int size) {
+    return articleService.listPage(page, size);
+  }
+
+  @RequestMapping("/getById")
+  public Article getById(Integer id) {
+    return articleService.getById(id);
   }
 }
