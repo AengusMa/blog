5831690926fa94480cb283570f5ba17034507436
mawenlong <15061886113@163.com>
2018-10-26 17:59:43 +0800

    删除测试方法

--- blog-portal-web/src/main/java/com/blog/article/controller/ArticleController.java
+++ blog-portal-web/src/main/java/com/blog/article/controller/ArticleController.java
@@ -19,19 +19,6 @@ public class ArticleController {
   @Reference
   private ArticleService articleService;
 
-  @RequestMapping("/test")
-  public void findCartList() {
-    Article article = new Article();
-    article.setModifydate(new Date());
-    article.setCreatedate(new Date());
-    article.setTitle("女孩都有浪漫的小情怀——浪漫的求婚词");
-    article.setContent("content");
-    article.setHits(22);
-    article.setPictureurl("images/2.jpg");
-    article.setSummary("还在为浪漫的求婚词而烦恼不知道该怎么说吗？女孩子都有着浪漫的小情怀，对于求婚更是抱着满满的浪漫期待，也希望在求婚那一天对方可以给自己一个最浪漫的求婚词。");
-    articleService.insert(article);
-  }
-
   @RequestMapping("/listPage")
   public PageResult listPage(int page, int size) {
     return articleService.listPage(page, size);
